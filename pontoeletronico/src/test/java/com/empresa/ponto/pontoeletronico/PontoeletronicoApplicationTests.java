7cb3c478eea7774014345aff11defe93d47a0c76
William Bacelar <william@vallus.com.br>
2026-01-18 16:54:58 -0300

    feat: first commit

---
+++ pontoeletronico/src/test/java/com/empresa/ponto/pontoeletronico/PontoeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.ponto.pontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
